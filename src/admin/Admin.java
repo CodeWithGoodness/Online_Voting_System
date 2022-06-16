011c8bbbf0fad0d431e4905b119d87ea637c805c
CodeWithGoodness <ceciliaOkoro2@gmail.com>
2022-06-16 03:30:30 -0700

    first commit

---
+++ src/admin/Admin.java
@@ -0,0 +1,50 @@
+package admin;
+
+public class Admin {
+    /*
+    Class Admin
+    This class has the following methods:
+    **RemoveCandidate: removes a candidate from the hashmap.
+    **CheckResult: iterate over and check the candidate with the largest vote.
+
+    Variables:
+    String password = constant.
+    date election date, candidates and voters registration opening/ closing date.
+
+
+    Class Voters
+    String Password,Name, Gender, State, dateRegistered;
+    Hashmap<username, password
+    int minAge  = 18;
+    Boolean Voted = false;
+    Register(){
+        Name,
+        String Password(also check if password already exists in the hashmap)
+        Age(compare with minAge)
+        Gender,
+        State,
+        dateRegistered;
+        **add username/password to hashmap here
+    }
+    Class candidates'
+    int minAge  = 35;
+     String Password,Name, Gender, party, State, dateRegistered;
+    Register(){
+        String Password,
+        Interested Position
+        Name,
+        Gender,
+        State,
+        Party,
+        dateRegistered;
+        SelfDescription
+    }
+    Display Profile(){
+    Name, Gender, party, State
+    }
+
+
+
+
+     */
+}
